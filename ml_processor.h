cbfcf3f66a2a7b565fadd662ffe50fd61583cc70
Felipe Rangel <feliperangelsilvestre@gmail.com>
2025-07-22 22:07:58 -0400

    Update ml_processor.h

--- ml_processor.h
+++ ml_processor.h
@@ -1,24 +1,28 @@
 /*
- * ml_processor.h - Header para processador ML
+ * ml_processor.h - Header corrigido para processador ML
  */
 
 #ifndef ML_PROCESSOR_H
 #define ML_PROCESSOR_H
 
 #include <Arduino.h>
+#include "config.h"
 
-// Verificar se TensorFlow Lite está disponível
-#ifdef ESP32
-  // Definir se TF Lite está disponível baseado na biblioteca instalada
-  #define TFLITE_AVAILABLE 1
-#else
-  #define TFLITE_AVAILABLE 0
-#endif
-
+// Forward declarations condicionais
 #if TFLITE_AVAILABLE
-  #include "tensorflow/lite/c/common.h"
+  // Incluir apenas os headers necessários que existem
+  #if __has_include("tensorflow/lite/micro/micro_interpreter.h")
+    #include "tensorflow/lite/micro/micro_interpreter.h"
+    #include "tensorflow/lite/micro/all_ops_resolver.h"
+    #include "tensorflow/lite/schema/schema_generated.h"
+    #include "tensorflow/lite/version.h"
+  #elif __has_include("TensorFlowLite_ESP32.h")
+    #include "TensorFlowLite_ESP32.h"
+  #elif __has_include("TensorFlowLite.h")
+    #include "TensorFlowLite.h"
+  #endif
   
-  // Forward declarations para evitar includes pesados no header
+  // Forward declaration para evitar problemas de compilação
   namespace tflite {
     class MicroInterpreter;
   }
@@ -27,13 +31,12 @@
 class MLProcessor {
 public:
     MLProcessor();
-    ~MLProcessor();
     
     // Inicializar o processador ML
     bool begin();
     
     // Executar predição
-    // Retorna: classe predita (-1 se erro)
+    // Retorna: classe predita (0-3) ou -1 se erro
     int predict(const float* input_data, size_t length);
     
     // Obter confiança da última predição
@@ -47,23 +50,22 @@ public:
 
 private:
 #if TFLITE_AVAILABLE
-    // Ponteiros para componentes TF Lite
+    // Componentes TensorFlow Lite
     tflite::MicroInterpreter* interpreter;
-    
-    // Tensores de entrada e saída
     TfLiteTensor* input_tensor;
     TfLiteTensor* output_tensor;
     
-    // Arena de memória para tensores
-    uint8_t* tensor_arena;
-    
-    // Dados do modelo (deve ser definido externamente)
-    extern const unsigned char model_data[];
-    extern const int model_data_len;
+    // Arena de memória estática
+    static constexpr size_t kTensorArenaSize = 60 * 1024;
+    alignas(16) uint8_t tensor_arena[kTensorArenaSize];
 #endif
     
-    // Última confiança calculada
+    // Estado interno
     float last_confidence;
+    bool ml_initialized;
+    
+    // Métodos auxiliares
+    void simulate_prediction(int& result, float& confidence);
 };
 
 #endif // ML_PROCESSOR_H
