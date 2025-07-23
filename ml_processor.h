1154f9025517bc3f77e14a019935db0e84eab8d1
Felipe Rangel <feliperangelsilvestre@gmail.com>
2025-07-22 21:59:44 -0400

    Update ml_processor.h

--- ml_processor.h
+++ ml_processor.h
@@ -1,26 +1,69 @@
+/*
+ * ml_processor.h - Header para processador ML
+ */
+
 #ifndef ML_PROCESSOR_H
 #define ML_PROCESSOR_H
 
 #include <Arduino.h>
-#include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
+
+// Verificar se TensorFlow Lite está disponível
+#ifdef ESP32
+  // Definir se TF Lite está disponível baseado na biblioteca instalada
+  #define TFLITE_AVAILABLE 1
+#else
+  #define TFLITE_AVAILABLE 0
+#endif
+
+#if TFLITE_AVAILABLE
+  #include "tensorflow/lite/c/common.h"
+  
+  // Forward declarations para evitar includes pesados no header
+  namespace tflite {
+    class MicroInterpreter;
+  }
+#endif
 
 class MLProcessor {
 public:
     MLProcessor();
-
-    // Inicializa o modelo TinyML
+    ~MLProcessor();
+    
+    // Inicializar o processador ML
     bool begin();
-
-    // Executa inferência no buffer de entrada
+    
+    // Executar predição
+    // Retorna: classe predita (-1 se erro)
     int predict(const float* input_data, size_t length);
+    
+    // Obter confiança da última predição
+    float get_confidence() const;
+    
+    // Verificar se ML está disponível
+    bool is_available() const;
+    
+    // Imprimir informações do modelo
+    void print_model_info() const;
 
 private:
-    const tflite::Model* model;
+#if TFLITE_AVAILABLE
+    // Ponteiros para componentes TF Lite
     tflite::MicroInterpreter* interpreter;
-    TfLiteTensor* input;
-    TfLiteTensor* output;
+    
+    // Tensores de entrada e saída
+    TfLiteTensor* input_tensor;
+    TfLiteTensor* output_tensor;
+    
+    // Arena de memória para tensores
+    uint8_t* tensor_arena;
+    
+    // Dados do modelo (deve ser definido externamente)
+    extern const unsigned char model_data[];
+    extern const int model_data_len;
+#endif
+    
+    // Última confiança calculada
+    float last_confidence;
 };
 
 #endif // ML_PROCESSOR_H
